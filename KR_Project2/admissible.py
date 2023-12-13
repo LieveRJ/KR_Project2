f90682bc59ce96dc753038ddfc888ab855dfddb2
thezamp <g.zamprogno@vu.nl>
2023-12-13 14:14:41 +0100

    some fixes to admissible.py

--- KR_Project2/admissible.py
+++ KR_Project2/admissible.py
@@ -7,21 +7,22 @@ def is_admissible(label):
     # if label is admissible then return true.
     if all(label[x] != 'mustOut' for x in label):
         return True
-    
+
     # Check if label is hopeless.
     for x in label:
-        if all(label[y] in {'out, mustOut', 'und'} for y in [attacker for (attacker, attacked) in framework if attacked == x]):
+        if label[x] == 'mustOut' and all(label[y] in {'out', 'mustOut', 'und'} for y in [attacker for (attacker, attacked) in framework if attacked == x]):
             return False
 
     # label1 ← label
     label1 = label.copy()
 
     # select some x ∈ {y | label(y) = mustOut} − with label(x) = blank.
-    x = [y for y in label if label[y] == 'mustOut' and label1[x] == 'blank']
-
+    #x = [y for y in label if label[y] == 'mustOut' and label1[x] == 'blank']
+    # should work if not hopeless
+    x = [attacker for (attacker, attacked) in framework if label[attacked] == 'mustOut' and label[attacker] == 'blank'][0]
     if x:
         in_trans(label1, x[0])
-        
+
         # if isAdmissible(label1)=true then return true.
         if is_admissible(label1):
             return True
@@ -40,15 +41,16 @@ def propagate(label):
         Select some x with label(x) = blank such that for all 
         y ∈ {x}− label(y) ∈ {out,mustOut}"""
         candidates = [x for x in label if label[x] == 'blank']
- 
+
         for x in candidates:
             # Checks conditions for all y ∈ {x}− label(y) ∈ {out, mustOut}
             if all(label[y] in {'out', 'mustOut'} for y in [attacker for (attacker, attacked) in framework if attacked == x]):
                 in_trans(label, x)
+                break #the for loop, the while continues
         else:
             """If there is no x with label(x) = blank such that for 
             all y ∈ {x} − label(y) ∈ {out,mustOut} then halt."""
-            break
+            return
 
 def in_trans(label, x):
     label[x] = 'in'
@@ -61,10 +63,11 @@ def in_trans(label, x):
     for y in [attacker for (attacker, attacked) in framework if attacked == x]:
         if label[y] != 'out':
             label[y] = 'mustOut'
+    return
 
 def und_trans(label, x):
     if label[x] == 'blank':
-        label[x] = 'und'    
+        label[x] = 'und'
 
 # credulous accepted arguments for preferred semantics.
 def credulous_accepted_arguments(label):
@@ -76,7 +79,7 @@ def credulous_accepted_arguments(label):
 
 def main(arg, args, framework):
     """
-    if the query argument s is self-attacking, then we conclude with arg being 
+    if the query argument s is self-attacking, then we conclude with arg being
     inadmissible
     """
     if (arg, arg) in framework:
@@ -90,7 +93,7 @@ def main(arg, args, framework):
     label = {}
     for x in args:
         label[x] = 'blank'
-    
+
     for x in label:
         if (x, x) in framework:
             label[x] = 'und'
@@ -102,7 +105,7 @@ def main(arg, args, framework):
         print('Yes')
     else:
         print('No')
-    
+
 
 # args = set(['a', 'b', 'c', 'd', 'e'])
 # framework = [('a','b'),('b','a'), ('b','c'), ('c','d'),('d','e'),('e','c')]
